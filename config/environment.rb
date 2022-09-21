7eab68abb421d575d6bc0991f2e1e29a604ef727
Evan Sailer <sailere@upmc.edu>
2022-09-21 09:53:43 -0700

    Configure gmail for verification emails

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
+  :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => ENV['GMAIL_USERNAME'],
+  :password => ENV['GMAIL_PASSWORD'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
